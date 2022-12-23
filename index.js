15d9dd3fe306336fdbf1a388c149e223ed45c071
Ansh <anshagrawal12348@gmail.com>
2022-12-23 13:46:51 +0530

    Updating Files

--- index.js
+++ index.js
@@ -100,3 +100,23 @@ function openForm() {
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
+
+function reveal(){
+    var reveals=document.querySelectorAll(".reveal");
+
+    reveals.forEach((reveal)=>{
+        var windowHeight=window.innerHeight;
+        var elementTop=reveal.getBoundingClientRect().top;
+        var elementVisible=100;
+
+        if (elementTop<windowHeight-elementVisible) {
+            reveal.classList.add("active");
+            
+        }else{
+            reveal.classList.remove("active");
+        }
+    });
+
+}
+window.addEventListener("scroll",reveal)
+
