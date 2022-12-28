751bde9f51a51aa3d695a1c773a1f3c33876eca5
Ansh <anshagrawal12348@gmail.com>
2022-12-28 22:58:32 +0530

    Updating Files

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var alert = require('alert');
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/donation');
+    await mongoose.connect('mongodb://127.0.0.1:27017/donation');
 
     // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
 }
